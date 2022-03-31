08fe8d28a68f7059ef4c3f2747bf65ae34eea30d
onlyhappiness <onlyhappiness@gmail.com>
2022-03-31 18:11:11 +0900

    [UPDATE] Login Screen

--- app/App.js
+++ app/App.js
@@ -1,29 +1,13 @@
-import * as React from 'react';
-import {Appbar} from 'react-native-paper';
-import {StyleSheet} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
+import React from 'react';
 
-const MyComponent = () => (
-  <Appbar style={styles.bottom}>
-    <Appbar.Action
-      icon="archive"
-      onPress={() => console.log('Pressed archive')}
-    />
-    <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
-    <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
-    <Appbar.Action
-      icon="delete"
-      onPress={() => console.log('Pressed delete')}
-    />
-  </Appbar>
-);
+import Login from './screens/login';
 
-export default MyComponent;
-
-const styles = StyleSheet.create({
-  bottom: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-});
+export default function App() {
+  return (
+    <View>
+      <Login />
+      {/* <TextInput placeholder="어떻게 나옴" /> */}
+    </View>
+  );
+}
--- app/screens/login.js
+++ app/screens/login.js
@@ -1,16 +1,85 @@
 /* eslint-disable */
-import {View, Text, TextInput, Button} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Pressable,
+  TextInput,
+} from 'react-native';
+import React, {useState} from 'react';
+
+// import {TextInput, } from 'react-native-paper';
 
 function Login() {
-  return (
-    <View>
-      <TextInput placeholder="useless placeholder" />
-      <TextInput placeholder="useless placeholder" />
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [phone, setPhone] = useState('');
 
-      <Button title="버튼" />
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text style={styles.logo}>LOGO</Text>
+        <TextInput
+          // flat
+          style={styles.textinput}
+          label={'Enter Email'}
+          activeUnderlineColor={'#C0C0C3'}
+          value={email}
+          onChangeText={text => setEmail(text)}
+        />
+        <TextInput
+          // flat
+          style={styles.textinput}
+          label={'Password'}
+          activeUnderlineColor={'#C0C0C3'}
+          value={password}
+          onChangeText={text => setPassword(text)}
+        />
+
+        {/* <Button style={styles.loginButton} title="Login" /> */}
+        <Pressable style={styles.loginButton}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
 export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F4EFF3',
+    height: '100%',
+  },
+  innerContainer: {
+    padding: 25,
+  },
+  logo: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginBottom: 25,
+  },
+  textinput: {
+    marginBottom: 20,
+    backgroundColor: '#ffffff',
+  },
+  loginButton: {
+    marginTop: 30,
+    marginBottom: 30,
+    backgroundColor: '#FF995E',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
