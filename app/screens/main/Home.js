cd617ea4c65e3b3499174e006a9730f9a78c2a02
onlyhappiness <onlyhappiness96@gmail.com>
2022-04-16 22:38:50 +0900

    [ MODIFY ] Home Screen

--- app/screens/main/Home.js
+++ app/screens/main/Home.js
@@ -1,11 +1,8 @@
-/* eslint-disable */
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
 import React from 'react';
-
 import LinearGradient from 'react-native-linear-gradient';
 
-
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <View
       style={{
@@ -20,8 +17,7 @@ const Home = () => {
           borderBottomRightRadius: 20,
           paddingHorizontal: 20,
         }}>
-        {/* FIXME: 아이콘을 놓을 위치 */}
-        {/* <Text>Drawer을 놓을 위치(아이콘)</Text> */}
+        <Text>리스트</Text>
 
         <View
           style={{
@@ -37,26 +33,96 @@ const Home = () => {
                 color: '#fff',
                 fontWeight: 'bold',
               }}>
-              Welcome~
+              Welcome~!
             </Text>
           </View>
 
-          <View style={{width: '50%', alignItems: 'flex-end'}}>
-            <Image
-              source={require('../../image/Home/pet.png')}
-              style={{height: 60, width: 60, borderRadius: 20}}
-            />
+          <View style={{width: '50%', alignItems: 'center'}}>
+            <Text>로고 넣을 자리</Text>
           </View>
         </View>
       </View>
 
-      <LinearGradient>
-
+      <LinearGradient
+        colors={['rgba(255, 153, 94,0.4)', 'transparent']}
+        style={{
+          left: 0,
+          right: 0,
+          height: 90,
+          marginTop: -45,
+        }}>
+        <View
+          style={{
+            backgroundColor: '#fff',
+            paddingVertical: 8,
+            paddingHorizontal: 20,
+            marginHorizontal: 20,
+            borderRadius: 15,
+            marginTop: 25,
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+          <TextInput
+            placeholder="Search"
+            placeholderTextColor="#b1e5d3"
+            style={{
+              fontWeight: 'bold',
+              fontSize: 18,
+              width: 260,
+            }}
+          />
+          <Text>검색 아이콘</Text>
+        </View>
       </LinearGradient>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          paddingHorizontal: 20,
+          width: '100%',
+          alignItems: 'center',
+        }}>
+        <View style={{width: '50%'}}>
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 17,
+              color: '#585a61',
+            }}>
+            Recommended
+          </Text>
+          <View
+            style={{
+              height: 4,
+              backgroundColor: '#b1e5d3',
+              width: 115,
+              marginTop: -5,
+            }}></View>
+        </View>
+
+        <View style={{width: '50%', alignItems: 'flex-end'}}>
+          <View
+            style={{
+              backgroundColor: '#00a46c',
+              paddingHorizontal: 20,
+              paddingVertical: 5,
+              borderRadius: 15,
+            }}>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 13,
+                color: '#fff',
+              }}>
+              More
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      {/* TODO: SCROLLVIEW */}
     </View>
   );
 };
-
-
 
 export default Home;
